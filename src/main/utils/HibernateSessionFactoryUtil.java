9825a9d76c4508266fafcc31285a35ee2e39dd3e
N.Y <kuzinaanastasia2210@gmail.com>
2025-09-24 15:18:25 +0400

    v1

---
+++ src/main/utils/HibernateSessionFactoryUtil.java
@@ -0,0 +1,38 @@
+package utils;
+
+import models.User;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+
+public class HibernateSessionFactoryUtil {
+    private static SessionFactory sessionFactory;
+
+    private HibernateSessionFactoryUtil() {}
+
+    public static SessionFactory getSessionFactory() {
+        if (sessionFactory == null) {
+            try {
+                Configuration configuration = new Configuration();
+                configuration.configure("hibernate.cfg.xml");
+                configuration.addAnnotatedClass(User.class);
+
+                StandardServiceRegistryBuilder builder = new StandardServiceRegistryBuilder()
+                        .applySettings(configuration.getProperties());
+
+                sessionFactory = configuration.buildSessionFactory(builder.build());
+
+            } catch (Exception e) {
+                System.err.println("Initial SessionFactory creation failed: " + e);
+                throw new ExceptionInInitializerError(e);
+            }
+        }
+        return sessionFactory;
+    }
+
+    public static void shutdown() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+    }
+}
