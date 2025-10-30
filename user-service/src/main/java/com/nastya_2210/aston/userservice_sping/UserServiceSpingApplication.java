9886d1a18aec9b4cd30d5a73503b6b5e5683e197
N.Y <kuzinaanastasia2210@gmail.com>
2025-10-30 14:26:27 +0400

    Докер

--- user-service/src/main/java/com/nastya_2210/aston/userservice_sping/UserServiceSpingApplication.java
+++ user-service/src/main/java/com/nastya_2210/aston/userservice_sping/UserServiceSpingApplication.java
@@ -4,10 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @RefreshScope
+@EnableRetry
 public class UserServiceSpingApplication {
 
     public static void main(String[] args) {
